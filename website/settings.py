acc0e0a8c93a3ba28a1ac2749ed2eed869ae4781
Arutoru <arthurtankwa@gmail.com>
2023-06-27 13:28:57 +0100

    Setting DEBUG to False

--- website/settings.py
+++ website/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 
 STATIC_URL = '/static/'
